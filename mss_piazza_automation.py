2ba4f5777e747ed59bdef0a6a0cfd73813053ba7
li-jiang-yan <48854965+li-jiang-yan@users.noreply.github.com>
2021-03-22 22:19:15 +0800

    Update mss_piazza_automation.py

--- mss_piazza_automation.py
+++ mss_piazza_automation.py
@@ -8,7 +8,7 @@ my_piazza_password = "" # Please put your password (that you use to login to Pia
 p = Piazza()
 p.user_login(email = my_piazza_email, password = my_piazza_password)
 mss = p.network("kizrj7sr77d2w6") # Enter Piazza network here (can be seen from Piazza URL)
-folder_list = ['hw1', 'hw2', 'hw3', 'hw4', 'hw5', 'hw6', 'hw7', 'hw8', 'hw9', 'hw10', 'project', 'exam', 'logistics', 'other', 'math_modelling', 'cerebry', 'week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7', 'week8', 'week9', 'week10', 'week11', 'week12', 'week13' ]
+folder_list = ['hw1', 'hw2', 'hw3', 'hw4', 'hw5', 'hw6', 'hw7', 'hw8', 'hw9', 'hw10', 'project', 'exam', 'logistics', 'other', 'week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7', 'week8', 'week9', 'week10', 'week11', 'week12', 'week13', 'math_modelling', 'cerebry' ]
 post_index_list = []
 post_dic = {}
 for foldername in folder_list:
@@ -37,7 +37,8 @@ print('\n')
 print('Copy + Paste From Here Onwards')
 print('--------------------------------------------------')
 print('AUTOMATED POST LIST')
-print('Source code: {}')
+print('Response to @393_f1 - if we all can take turns to just run this code on our computers and update this information post regularly, we can save the class a lot of trouble. You just, need to run this, and copy + paste the outputs...')
+print('Source code: https://github.com/li-jiang-yan/mss-ta-code/blob/main/mss_piazza_automation.py')
 print('Last updated: {}'.format(time.asctime()))
 
 for foldertitle in post_dic.keys():
@@ -45,3 +46,6 @@ for foldertitle in post_dic.keys():
     print(foldertitle)
     for current_post in post_dic[foldertitle]:
         print("{} : @{}".format(current_post["history"][0]["subject"], current_post["nr"]))
+
+print('\n')
+print('--- END OF AUTOMATED POST LIST ---')
